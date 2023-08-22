9c61a12bec4278aba4df53d4360f54e334ebca6e
markhornchurch <142870708+markhornchurch@users.noreply.github.com>
2023-08-22 07:37:15 +0100

    Update streamlit_app.py
    added text from exercise

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy diner')
-streamlit.text('blah')
-streamlit.text('blah')
-streamlit.text('blah fff')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
